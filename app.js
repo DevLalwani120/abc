9fdce3f50b7bc534732dd3d90359f5658b4a503a
Your Name <you@example.com>
2024-02-11 19:58:26 +0530

    second commit

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const http=require('http');
 var port=3000;
+var hostname='127.0.0.1'
 http.createServer((req,res)=>{
     
     res.writeHead(200,{'Content-Type':'text/html'})
     res.write(`<h1>Server created SuccessFully</h1>
     <h2>HELLO WORLD</h2>`)
     res.end()
-}).listen(port,()=>{
-    console.log(`Server running at http://localhost:${port}/`)
+}).listen(port,hostname,()=>{
+    console.log(`Server running at http://${hostname}:${port}/`)
 });
